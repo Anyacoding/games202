ad702926e26628c62fad169ecc10f36213815cbc
Anyacoding <1710273192@qq.com>
2023-09-16 10:35:46 +0800

    finish thr homework2

---
+++ homework2/prt/build/CMakeFiles/nori.dir/DependInfo.cmake
@@ -0,0 +1,77 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/anya/games202/homework2/prt/ext/spherical-harmonics/sh/default_image.cc" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/ext/spherical-harmonics/sh/default_image.cc.o"
+  "/home/anya/games202/homework2/prt/ext/spherical-harmonics/sh/spherical_harmonics.cc" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/ext/spherical-harmonics/sh/spherical_harmonics.cc.o"
+  "/home/anya/games202/homework2/prt/src/accel.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/accel.cpp.o"
+  "/home/anya/games202/homework2/prt/src/bitmap.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/bitmap.cpp.o"
+  "/home/anya/games202/homework2/prt/src/block.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/block.cpp.o"
+  "/home/anya/games202/homework2/prt/src/chi2test.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/chi2test.cpp.o"
+  "/home/anya/games202/homework2/prt/src/common.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/common.cpp.o"
+  "/home/anya/games202/homework2/prt/src/dielectric.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/dielectric.cpp.o"
+  "/home/anya/games202/homework2/prt/src/diffuse.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/diffuse.cpp.o"
+  "/home/anya/games202/homework2/prt/src/gui.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/gui.cpp.o"
+  "/home/anya/games202/homework2/prt/src/independent.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/independent.cpp.o"
+  "/home/anya/games202/homework2/prt/src/main.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/main.cpp.o"
+  "/home/anya/games202/homework2/prt/src/mesh.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/mesh.cpp.o"
+  "/home/anya/games202/homework2/prt/src/microfacet.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/microfacet.cpp.o"
+  "/home/anya/games202/homework2/prt/src/mirror.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/mirror.cpp.o"
+  "/home/anya/games202/homework2/prt/src/obj.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/obj.cpp.o"
+  "/home/anya/games202/homework2/prt/src/object.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/object.cpp.o"
+  "/home/anya/games202/homework2/prt/src/parser.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/parser.cpp.o"
+  "/home/anya/games202/homework2/prt/src/perspective.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/perspective.cpp.o"
+  "/home/anya/games202/homework2/prt/src/proplist.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/proplist.cpp.o"
+  "/home/anya/games202/homework2/prt/src/prt.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/prt.cpp.o"
+  "/home/anya/games202/homework2/prt/src/rfilter.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/rfilter.cpp.o"
+  "/home/anya/games202/homework2/prt/src/scene.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/scene.cpp.o"
+  "/home/anya/games202/homework2/prt/src/ttest.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/ttest.cpp.o"
+  "/home/anya/games202/homework2/prt/src/warp.cpp" "/home/anya/games202/homework2/prt/build/CMakeFiles/nori.dir/src/warp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "NANOGUI_USE_OPENGL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../ext/tinyformat"
+  "../ext/eigen"
+  "../ext/openexr/IlmBase/Imath"
+  "../ext/openexr/IlmBase/Iex"
+  "../ext/openexr/IlmBase/Half"
+  "../ext/openexr/OpenEXR/IlmImf"
+  "ext_build/openexr/OpenEXR/config"
+  "ext_build/openexr/IlmBase/config"
+  "../ext/tbb/include"
+  "../ext/pcg32"
+  "../ext/pugixml/src"
+  "../ext/hypothesis"
+  "../ext/nanogui/ext/glfw/include"
+  "../ext/nanogui/ext/glew/include"
+  "../ext/nanogui/ext/nanovg/src"
+  "../ext/nanogui/include"
+  "../ext/filesystem"
+  "../ext/nanogui/ext/nanovg/example"
+  "../ext/spherical-harmonics"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/anya/games202/homework2/prt/build/ext_build/tbb/CMakeFiles/tbb_static.dir/DependInfo.cmake"
+  "/home/anya/games202/homework2/prt/build/ext_build/CMakeFiles/pugixml.dir/DependInfo.cmake"
+  "/home/anya/games202/homework2/prt/build/ext_build/openexr/OpenEXR/IlmImf/CMakeFiles/IlmImf.dir/DependInfo.cmake"
+  "/home/anya/games202/homework2/prt/build/ext_build/nanogui/CMakeFiles/nanogui.dir/DependInfo.cmake"
+  "/home/anya/games202/homework2/prt/build/ext_build/openexr/IlmBase/Half/CMakeFiles/Half.dir/DependInfo.cmake"
+  "/home/anya/games202/homework2/prt/build/ext_build/openexr/IlmBase/Imath/CMakeFiles/Imath.dir/DependInfo.cmake"
+  "/home/anya/games202/homework2/prt/build/ext_build/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/DependInfo.cmake"
+  "/home/anya/games202/homework2/prt/build/ext_build/openexr/IlmBase/Iex/CMakeFiles/Iex.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
